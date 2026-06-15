d853438bfdb75f9884ac2fdd4064a5b7225614c0
MilesN29 <mdn29@pitt.edu>
2026-06-15 11:33:41 -0400

    automated jenkins build

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello from Jenkins CI/CD!'
 
+@app.route('/test')
+def test():
+    return 'Successful test!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
